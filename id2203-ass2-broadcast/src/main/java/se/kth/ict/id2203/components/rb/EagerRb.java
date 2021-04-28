297bca92d432a533703cd6a7d22f8217d7797e54
Anya Sendzyak <sendzyak2017@gmail.com>
2021-04-28 22:51:40 +0700

    2  lab

---
+++ id2203-ass2-broadcast/src/main/java/se/kth/ict/id2203/components/rb/EagerRb.java
@@ -0,0 +1,16 @@
+package se.kth.ict.id2203.components.rb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.sics.kompics.ComponentDefinition;
+
+public class EagerRb extends ComponentDefinition {
+
+	private static final Logger logger = LoggerFactory.getLogger(EagerRb.class);
+
+	public EagerRb(EagerRbInit init) {
+
+	}
+
+}
---
+++ id2203-ass2-broadcast/src/main/java/se/kth/ict/id2203/pa/broadcast/AutomaticCorrection.java
@@ -0,0 +1,11 @@
+package se.kth.ict.id2203.pa.broadcast;
+
+import se.kth.ict.id2203.sim.broadcast.BroadcastTester;
+
+public class AutomaticCorrection {
+	public static void main(String[] args) {
+		String email = "enter email here";
+		String password = "enter password here";
+		BroadcastTester.correctAndSubmit(email, password);
+	}
+}
