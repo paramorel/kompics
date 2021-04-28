51f690278ed29ae318dc6411fcac8ec8948c38a9
Anya Sendzyak <sendzyak2017@gmail.com>
2021-04-28 21:55:47 +0700

    0 lab

---
+++ introductionToKompics/src/main/java/se/kth/ict/id2203/pa/start/AutomaticCorrection.java
@@ -0,0 +1,11 @@
+package se.kth.ict.id2203.pa.start;
+
+import se.kth.ict.id2203.sim.start.StartTester;
+
+public class AutomaticCorrection {
+	public static void main(String[] args) {
+		String email = "sendzyak2017@yandex.ru";
+		String password = "azaz1964";
+		StartTester.correctAndSubmit(email, password);
+	}
+}
