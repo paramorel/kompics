66296a8406b718713504d290d20c9f4f96b3054e
Anya Sendzyak <sendzyak2017@gmail.com>
2021-04-28 23:46:59 +0700

    3с lab

---
+++ id2203-ass3-shm/src/main/java/se/kth/ict/id2203/components/riwcm/ReadImposeWriteConsultMajority.java
@@ -0,0 +1,16 @@
+package se.kth.ict.id2203.components.riwcm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.sics.kompics.ComponentDefinition;
+
+public class ReadImposeWriteConsultMajority extends ComponentDefinition {
+
+	private static final Logger logger = LoggerFactory.getLogger(ReadImposeWriteConsultMajority.class);
+
+	public ReadImposeWriteConsultMajority(ReadImposeWriteConsultMajorityInit event) {
+
+	}
+
+}
